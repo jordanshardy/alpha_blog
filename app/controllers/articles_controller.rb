dd33d4e9bab86467f670dea17c90add3207bca12
jshardy13 <jordan.s.hardy@gmail.com>
2021-06-23 19:03:42 +1000

    added new and create routes and new.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,14 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+
+  end
+
+  def create
+    render plain: params[:article]
+  end
+
   def show
     id = params[:id]
     @article = Article.find {|item| item[:id] == id.to_i}
